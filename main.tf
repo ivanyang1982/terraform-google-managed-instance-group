59e172a0b2ebb229c98da1a8e5a1320f8a23bc97
ivanyang1982 <35478187+ivanyang1982@users.noreply.github.com>
2020-01-23 16:05:29 +0800

    update main.tf for correcting some variable presentations.

--- main.tf
+++ main.tf
@@ -23,14 +23,14 @@ resource "google_compute_instance_template" "default" {
 
   region = "${var.region}"
 
-  tags = ["${concat(list("allow-ssh"), var.target_tags)}"]
+  tags = "${concat(list("allow-ssh"), var.target_tags)}"
 
   labels = "${var.instance_labels}"
 
   network_interface {
     network            = "${var.subnetwork == "" ? var.network : ""}"
     subnetwork         = "${var.subnetwork}"
-    access_config      = ["${var.access_config}"]
+    access_config      = "${var.access_config}"
     address            = "${var.network_ip}"
     subnetwork_project = "${var.subnetwork_project == "" ? var.project : var.subnetwork_project}"
   }
@@ -49,7 +49,7 @@ resource "google_compute_instance_template" "default" {
 
   service_account {
     email  = "${var.service_account_email}"
-    scopes = ["${var.service_account_scopes}"]
+    scopes = "${var.service_account_scopes}"
   }
 
   metadata = "${merge(
@@ -239,7 +239,7 @@ resource "google_compute_firewall" "default-ssh" {
     ports    = ["22"]
   }
 
-  source_ranges = ["${var.ssh_source_ranges}"]
+  source_ranges = "${var.ssh_source_ranges}"
   target_tags   = ["allow-ssh"]
 }
 
@@ -271,7 +271,7 @@ resource "google_compute_firewall" "mig-health-check" {
   }
 
   source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
-  target_tags   = ["${var.target_tags}"]
+  target_tags   = "${var.target_tags}"
 }
 
 data "google_compute_instance_group" "zonal" {
